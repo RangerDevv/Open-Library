bb99207ce0e3b93ca4ef65d08106e46b96e51beb
Rangerdevv <meerabdul2008@gmail.com>
2022-07-05 16:57:45 -0400

    added book functionality

---
+++ src/dashboardmanager.js
@@ -0,0 +1,28 @@
+let CloneBook = document.getElementById("TestBook");
+let Shelf1 = document.getElementById("Shelf1");
+let Shelf2 = document.getElementById("Shelf2");
+
+let BookList =  [
+    {
+        "title": "Amogus",
+        "author": "John Doe",
+        "pages": "666",
+        "src": "./Books/Tutorial/TestBook.html"    
+    },
+    {
+        "title": "hell (beep)",
+        "author": "the (beep)",
+        "pages": "1",
+        "src": "./Books/Tutorial/TestBook.html"
+    },
+]
+
+let AmogusBook = CloneBook.cloneNode(true);
+AmogusBook.setAttribute("href", BookList[0].src);
+Shelf2.appendChild(AmogusBook);
+AmogusBook.firstChild.lastChild.innerText = BookList[0].title;
+
+let Newbook = CloneBook.cloneNode(true);
+Newbook.setAttribute("href", BookList[1].src);
+Shelf2.appendChild(Newbook);
+Newbook.firstChild.lastChild.innerText = BookList[1].title;
