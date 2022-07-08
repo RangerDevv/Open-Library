ebd569bb22280022f42f173a7a8dab1534b65b4b
Rangerdevv <meerabdul2008@gmail.com>
2022-07-08 12:43:02 -0400

    changes to some of the book

--- src/dashboardmanager.js
+++ src/dashboardmanager.js
@@ -4,14 +4,14 @@ let Shelf2 = document.getElementById("Shelf2");
 
 let BookList =  [
     {
-        "title": "Amogus",
+        "title": "Amogus - The Book of Amogus",
         "author": "John Doe",
         "pages": "666",
         "src": "./Books/Tutorial/Shelf.html"    
     },
     {
-        "title": "doctor",
-        "author": "meer",
+        "title": "Harry Potter and the Sorcerer's Stone",
+        "author": "J.K. Rowling",
         "pages": "0987",
         "src": "./Books/Tutorial/TestBook.html"
     }
@@ -20,9 +20,11 @@ let BookList =  [
 let AmogusBook = CloneBook.cloneNode(true);
 AmogusBook.setAttribute("href", BookList[0].src);
 Shelf2.appendChild(AmogusBook);
-AmogusBook.firstChild.lastChild.innerText = BookList[0].title;
+AmogusBook.firstChild.firstChild.innerText = BookList[0].title;
+AmogusBook.firstChild.lastChild.innerText = BookList[0].author;
 
 let Newbook = CloneBook.cloneNode(true);
 Newbook.setAttribute("href", BookList[1].src);
 Shelf2.appendChild(Newbook);
-Newbook.firstChild.lastChild.innerText = BookList[1].title;
+Newbook.firstChild.firstChild.innerText = BookList[1].title;
+Newbook.firstChild.lastChild.innerText = BookList[1].author;
