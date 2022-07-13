a8a55079698e9c03af44f8c543587477c938099f
Rangerdevv <meerabdul2008@gmail.com>
2022-07-13 10:44:29 -0400

    Added shelf managment system and bug fixes

---
+++ src/books/Tutorial/Shelfcont.js
@@ -0,0 +1,31 @@
+let CloneBook = document.getElementById("TestBook");
+let Shelf1 = document.getElementById("Shelf1");
+let Shelf2 = document.getElementById("Shelf2");
+let Shelf3 = document.getElementById("Shelf3");
+
+let BookList =  [
+    {
+        "title": "About Open Library",
+        "author": "John Doe",
+        "pages": "666",
+        "src": "./Books/Tutorial/TestBook.html"    
+    },
+    {
+        "title": "Another Tutorial",
+        "author": "John Doe",
+        "pages": "666",
+        "src": "./Books/Tutorial/TestBook.html"
+    }
+]
+
+let TutorialBook = CloneBook.cloneNode(true);
+TutorialBook.setAttribute("href", BookList[0].src);
+Shelf3.appendChild(TutorialBook);
+TutorialBook.firstChild.firstChild.innerText = BookList[0].title;
+TutorialBook.firstChild.lastChild.innerText = BookList[0].author;
+
+let AnotherBook = CloneBook.cloneNode(true);
+AnotherBook.setAttribute("href", BookList[1].src);
+Shelf2.appendChild(AnotherBook);
+AnotherBook.firstChild.firstChild.innerText = BookList[1].title;
+AnotherBook.firstChild.lastChild.innerText = BookList[1].author;
