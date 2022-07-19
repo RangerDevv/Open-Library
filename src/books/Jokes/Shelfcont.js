0b2a6cae69e11f5f73eaded3c806fc9d1d12c7eb
Rangerdevv <meerabdul2008@gmail.com>
2022-07-19 11:49:28 -0400

    added shelf to every singe cat

---
+++ src/books/Jokes/Shelfcont.js
@@ -0,0 +1,7 @@
+let CloneBook = document.getElementById("TestBook");
+let Shelf1 = document.getElementById("Shelf1");
+let Shelf2 = document.getElementById("Shelf2");
+let Shelf3 = document.getElementById("Shelf3");
+
+let BookList =  [
+]
---
+++ src/books/Literature/Shelfcont.js
@@ -0,0 +1,8 @@
+let CloneBook = document.getElementById("TestBook");
+let Shelf1 = document.getElementById("Shelf1");
+let Shelf2 = document.getElementById("Shelf2");
+let Shelf3 = document.getElementById("Shelf3");
+
+let BookList =  [
+]
+
