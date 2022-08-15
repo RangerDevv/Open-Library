591b6080101082df7f86b908635ebaa3a970e7fc
Rangerdevv <meerabdul2008@gmail.com>
2022-08-15 13:54:35 -0400

    Added new book [Metamorphosis]

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #user/bin/bash
 echo "Updating..."
 git add .
-git commit -m "Added new book"
+git commit -m "Added new book [Metamorphosis]"
 git push
 echo "Done!"
--- src/books/Horror/Shelfcont.js
+++ src/books/Horror/Shelfcont.js
@@ -4,5 +4,15 @@ let Shelf2 = document.getElementById("Shelf2");
 let Shelf3 = document.getElementById("Shelf3");
 
 let BookList =  [
+    {
+        "title": "Metamor\nphosis",
+        "author": "Franz Kafka",
+        "src": "./Books/Metamorphosis/Bookinfo.html"
+    }
 ]
 
+let Metamorphosis = CloneBook.cloneNode(true);
+Metamorphosis.setAttribute("href", BookList[0].src);
+Shelf1.appendChild(Metamorphosis);
+Metamorphosis.firstChild.firstChild.innerText = BookList[0].title;
+Metamorphosis.firstChild.lastChild.innerText = BookList[0].author;
